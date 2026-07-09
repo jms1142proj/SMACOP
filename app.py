bd8847a8dfd5f3be5faacca62acb7e1c501bc88b
jackpham1019 <jackpham1019@gmail.com>
2026-07-09 11:54:06 -0500

    feat: add skeleton for azure deployment flow (#1)
    Co-authored-by: Quang Pham <quangpham1019@gmail.com>

---
+++ app.py
@@ -0,0 +1,7 @@
+
+from infra.azure_flow import start_deployment
+
+if __name__ == "__main__":
+    # display interactive cli, with deployment being an option
+    
+    start_deployment()
---
+++ infra/azure_flow.py
@@ -0,0 +1,45 @@
+
+
+def authenticate():
+    # Authenticate the python automation script with azure using service principal
+    pass
+
+def create_resource_group():
+    pass
+
+def create_vm():
+    pass
+
+def create_azure_web_app():
+    pass
+
+def create_nsg():
+    pass
+
+def create_vnet():
+    pass
+
+def create_subnet():
+    pass
+
+def create_service_endpoint():
+    pass
+
+def create_private_endpoint():
+    pass
+
+def create_azure_monitor():
+    pass
+
+def create_log_analytics_workspace():
+    pass
+
+def create_grafana_dashboard():
+    pass
+
+def start_deployment():
+    # Begin the deployment
+    authenticate()
+
+    # Create resource group
+    pass
